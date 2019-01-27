b639d2ac03513e451123ad9f1fd434f18957f5f9
周庆猛 <zhouqingmeng@kedabeijing.com>
2019-01-27 21:24:53 +0800

    zzz

---
+++ style/js/json.js
@@ -0,0 +1,114 @@
+/**
+ * Created by zhouqm on 2017/12/27.
+ */
+var Json = {
+    project:[
+    {
+        name:"神州云动官网重构和维护",
+        date:"2017",
+        detail:" <br>神州云动官网网站：<br>1、使用nodejs+express进行重构。<br>2，把神州云动CRM平台的注册使用从CRM平台中迁移出来，通告nodejs服务注册登录。<br>3.开发新的官网页面",
+        image:["images/photo/7/1.png","images/photo/7/2.png","images/photo/7/3.png","images/photo/7/4.png"],
+        link:"www.cloudcc.com/"
+    },
+    {
+        name:"神州云动轻量级CRM 系统",
+        date:"2017.7-2018.1",
+        detail:"<br>神州云动轻量级CRM 系统：<br>1、负责神州云动轻量级CRM 系统开发；<br>2，使用ES6 + Antd Design + React + webpack热更新与打包编译；<br>3、前后端分离开发，接口使用RESTful API规范<br>4.与后端开发一起定义接口规范和测试；<br>5.神州云动轻量级CRM系统与cloudccCRM老版系统进行兼容；",
+        image:["images/photo/9/1.png","images/photo/9/2.png","images/photo/9/3.png","images/photo/9/4.png"],
+        link:"http://test.production.cloudcc.com//"
+    },
+    {
+        name:"神州云动pass平台cloudccCRM系统",
+        date:"2017.1-2017.6",
+        detail:"<br>神州云动pass平台cloudccCRM系统：<br>1、负责PC端页面排版和布局，及时与UI设计人员进行沟通交流，以保证排版的准确无误；<br>2，使用javascript，jQuery等技术进行网页效果制作；<br>3、.使用svn对项目进行管理；<br>4.负责配合后台端完成ajax请求、数据渲染；<br>5.使用gulp，webpack工具进行后期代码的压缩和优化；",
+        image:["images/photo/8/1.png","images/photo/8/2.png","images/photo/8/3.png","images/photo/8/4.png"],
+        link:"https://login.cloudcc.com/"
+    },
+    {
+        name:"暖岛网手机端",
+        date:"2015.04-2015.08",
+        detail:"<b>责任描述：</b><br>暖岛网是一家提供设计师产品折扣闪购的B2C购物网站，致力于为所有热爱生活的人提供最独特的创意生活，为消费者提供国内最优秀的设计产品，为品牌、设计师提供最直接优质的销售平台。<br>1、使用Fireworks进行页面切图和图片处理；<br>2、使用DIV+CSS进行页面布局，兼容各大主流浏览器。<br>3.使用Ajax与后台交互，实时获取数据，实现无限加载的效果;<br>4.使用less编译CSS，完成代码的优化，减少代码的沉余。",
+        image:["images/photo/1/01.png","images/photo/1/02.png","images/photo/1/03.png","images/photo/1/04.png"],
+        link:"www.nuandao.com"
+    },
+    {
+        name:"暖岛网",
+        date:"2014.09-2015.03",
+        detail:"<b>责任描述：</b><br>暖岛网是一家提供设计师产品折扣闪购的B2C购物网站，致力于为所有热爱生活的人提供最独特的创意生活，为消费者提供国内最优秀的设计产品，为品牌、设计师提供最直接优质的销售平台。<br>1、负责首页和分会场的DIV+CSS经典布局。<br>2.用iScroll插件实现区域滚动，可以跨手机版本实现弹性滚动，用来解决由于不同的手机版本造成页面的弹性滚动条兼容性不好的问题，提高不同手机版本的用户体验效果；并且配合ajax的数据请求实现页面的无限加载的效果。<br>3.Banner区域应用Swiper插件实现焦点图无缝定时轮播，使得轮播效果更加流畅，增强用户使用效果；<br>4.通过JQuery实现导航栏的滑入滑出效果以及焦点轮播图的无缝轮播。5.使用zepto.js和touch.js类库配合使用，控制页面中的DOM元素，加快移动端页面的加载速度。",
+        image:["images/photo/2/1.png","images/photo/2/2.png","images/photo/2/3.png","images/photo/2/4.png"],
+        link:"www.nuandao.com"
+    },
+    {
+        name:"临摹御龙在天官网",date:"2016.9",detail:"《御龙在天》三国历史背景，再现古代战争上万人真实对抗的宏伟战争场面。使用fullpage插件实现轮播图效果，使用jQuery快速写出动态效果，小图标较多，使用了雪碧图和字体图标，有利于加载。",
+        image:["images/photo/3/01.png","images/photo/3/02.png","images/photo/3/03.png","images/photo/3/04.png"],
+        link:"www.hanwuji.cn.com/yulong"
+    },
+    {
+        name:"selectedin（SaaS平台）和后台管理系统 ",
+        date:"2016.06-2016.12",
+        detail:"别具一格的的软件即服务（SaaS）解决方案，Selectedin旨在帮助采购人员更有效的采购/供应非标工业部件。<br><b>项目描述:</b><br>1.负责PC端页面排版和布局，及时与UI设计人员进行沟通交流，以保证排版的准确无误；<br>2.使用javascript，jQuery等技术进行网页效果制作；<br>3.使用git，svn对项目进行管理；<br>4.负责配合服务器端完成ajax请求、数据渲染；<br>5.使用angularjs，bower及其组件对项目进行开发；",
+        image:["images/photo/4/1.png","images/photo/4/2.png","images/photo/4/3.png","images/photo/4/4.png"],
+        link:"www.selectedin.com"
+    },
+    {
+        name:"淘宝首页",date:"2015.10",
+        detail:" 页面的制作整个基于rem单位。手机端的滑动比较阻塞，于是用了iScroll插件。网页效果整个基于jquery2.0，包括：banner的无缝滚动 、点击跳转链接（只做了部分处理），页面的图片的延迟加载，倒计时效果。",
+        image:["images/photo/5/01.png","images/photo/5/02.png","images/photo/5/03.png","images/photo/5/4.png"],
+        link:"www.hanwuji.cn.com/淘宝网"
+    },
+    {
+        name:"个人博客网站前台+后台",
+        date:"2016",
+        detail:" <b>项目描述：<br>个人博客网站，后台功能主要有：<br>1、登录注销控制，连接数据库进行登录账号和密码验证。<br>2，文章和文章分类的添加、修改、删除。</b><br>1、前台使用JavaScript+less+html5+css3+bootstrap+jQuery<br>2.后台使用ajax+josn+mongodb+node.js",
+        image:["images/photo/6/1.png","images/photo/6/2.png","images/photo/6/3.png","images/photo/6/4.png"],
+        link:"www.hanwuji.cn.com/"
+    }
+    ],
+    work:[
+    {
+        company:"北京神州云动科技股份有限公司",
+        job:"web前端开发工程师",
+        date:"2017年1月-2018年1月",
+        detail:"1.负责神州云动cloudccCRM系统开发；<br>2.负责神州云动cloudccCRM轻量级lightning系统开发；<br>3.负责神州云动官网www.cloudcc.com的维护和重构。<br>4.使用reactjs，ant-design及其组件对项目进行开发；<br>5.使用gulp，webpack工具进行后期代码的压缩和优化；<br>6.积极配合后台人员负责产品页面和功能的实现；持续的优化前端体验和页面响应速度，并保证各浏览器的兼容性和执行效率；",
+        logo:"images/icons/header1.png"
+    },
+    {
+        company:"北京维德信通科技有限公司",
+        job:"web前端开发工程师",
+        date:"2015年10月-2016年12月",
+        detail:"1.负责PC端页面排版和布局，及时与UI设计人员进行沟通交流，以保证排版的准确无误；<br>2.使用javascript，jQuery等技术进行网页效果制作；<br>3.使用git，svn对项目进行管理；<br>4.负责配合服务器端完成ajax请求、数据渲染；<br>5.使用angularjs，bower及其组件对项目进行开发；<br>6.使用gulp，webpack工具进行后期代码的压缩和优化；<br>7.积极配合后台人员负责产品页面和功能的实现；持续的优化前端体验和页面响应速度，并保证各浏览器的兼容性和执行效率；",
+        logo:"images/icons/header1.png"
+    },
+    {
+        company:"北京暖岛网络信息技术有限公司",
+        job:"web前端开发工程师",
+        date:"2014年08月-2015年09月",
+        detail:"1.使用javascript，bootstrap，zepto，iscroll，swiper等技术进行网页效果制作；<br>2.使用git，github，svn对项目进行管理；<br>3.使用angularjs，bower及其组件对项目进行开发；<br>4.使用gulp，webpack工具进行后期代码的压缩和优化；",
+        logo:"images/icons/header1.png"
+    },
+    {
+        company:"广电银通金融电子有限公司（实习）",
+        job:"清机服务工程师",
+        date:"2013年08月-2014年02月",
+        detail:"负责郑州市区中国银行ATM机的维修和保养",
+        logo:"images/icons/header1.png"
+    }
+    ],
+    myInfo:{
+        name:"周庆猛",
+        slogan:"3.5年前端经验",
+        edu:"本科",
+        time:"3.5年",
+        sex:"男",
+        city:"北京",
+        phone:"17610062723",
+        mail:"13523524394@126.com",
+        work:"web前端开发工程师 北京 全职 13K-16K",
+        work1:"前端工程师",
+        nature:"全职",
+        hopeCity:"北京",
+        money:"13K-16K",
+        head:"images/photo/head.jpg",ps:""
+    }
+
+};
